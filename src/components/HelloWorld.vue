3bd64fca6543927ecd44b98b8a42787c259ac6cb
cloud <you@example.com>
2022-07-19 00:08:10 +0800

    Changes to be committed: new file:   deploy.sh modified:   src/components/HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      該網站使用vue,spring boot開發
-    </p>
-    <h3>使用技術</h3>
+    <p>該網站使用vue,spring boot開發</p>
+    <h3>About project</h3>
+    使用vue & spring boot 練習開發，參考halo專案，增進專案經驗
+
+    <h3>function</h3>
     <ul>
-      <li>6584684684</li>
+      <li>類別與商品CRUD，類別設定cascade操作</li>
+      <li>商品分頁與價格排序</li>
+      <li>api 使用 filter ，新增修改需登入驗證 token</li>
+    </ul>
+
+    <h3>tools</h3>
+    <ul>
+      <li>vue cli</li>
+      <li>GCP VM用docker啟動server</li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -30,7 +39,7 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
+  list-style-type: none;
   margin: 0 10px;
   color: #42b983;
 }
